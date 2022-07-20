dad5cdb4794a8792985d3ff400618ea7d69bb194
supremazx <58238639+supremazx@users.noreply.github.com>
2022-07-20 03:23:53 +0300

    Update 40-dahdi-linux.sh

--- packages/40-dahdi-linux.sh
+++ packages/40-dahdi-linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LIB_VERSION='3.1.0';
-LIB_URL="https://downloads.asterisk.org/pub/telephony/dahdi-linux/dahdi-linux-${LIB_VERSION}.tar.gz";
+LIB_URL="https://downloads.asterisk.org/pub/telephony/dahdi-linux/dahdi-linux-3.1.0.tar.gz";
 srcDirName=$(downloadFile "$LIB_URL");
 (
   cd "$srcDirName" || exit;
