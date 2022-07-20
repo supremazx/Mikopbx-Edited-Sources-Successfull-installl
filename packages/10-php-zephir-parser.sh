58d9d547f49e87a0fc84494471af330ac9da89e4
supremazx <58238639+supremazx@users.noreply.github.com>
2022-07-20 03:23:17 +0300

    Update 10-php-zephir-parser.sh

--- packages/10-php-zephir-parser.sh
+++ packages/10-php-zephir-parser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LIB_VERSION='1.3.6';
-LIB_URL="https://github.com/zephir-lang/php-zephir-parser/archive/refs/tags/v${LIB_VERSION}.tar.gz";
+LIB_URL="https://github.com/zephir-lang/php-zephir-parser/archive/refs/tags/v1.3.6.tar.gz";
 LIB_PRIORITY='20';
 LIB_PHP_MODULE_PREFIX_INI='';
 LIB_CONFIGURE_OPTIONS='';
