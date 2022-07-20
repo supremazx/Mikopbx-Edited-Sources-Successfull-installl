2912c1cfb4730b0a07e56ff5a7c4195e6ad4aa8d
supremazx <58238639+supremazx@users.noreply.github.com>
2022-07-20 03:21:42 +0300

    Update 10-php-psr.sh

--- packages/10-php-psr.sh
+++ packages/10-php-psr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LIB_VERSION='1.0.1';
-LIB_URL="https://github.com/jbboehr/php-psr/archive/v${LIB_VERSION}.tar.gz";
+LIB_URL="https://github.com/jbboehr/php-psr/archive/v1.0.1.tar.gz";
 LIB_PRIORITY='40';
 LIB_PHP_MODULE_PREFIX_INI='';
 LIB_CONFIGURE_OPTIONS='';
