e9d6295305a995b33fb8393897f7d1047f1f6d07
supremazx <58238639+supremazx@users.noreply.github.com>
2022-07-20 03:20:52 +0300

    Update 10-php-igbinary.sh

--- packages/10-php-igbinary.sh
+++ packages/10-php-igbinary.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LIB_VERSION='3.2.1';
-LIB_URL="https://github.com/igbinary/igbinary/archive/refs/tags/${LIB_VERSION}.tar.gz";
+LIB_URL="https://github.com/igbinary/igbinary/archive/refs/tags/3.2.1.tar.gz";
 LIB_PRIORITY='40';
 LIB_PHP_MODULE_PREFIX_INI='';
 LIB_CONFIGURE_OPTIONS='';
