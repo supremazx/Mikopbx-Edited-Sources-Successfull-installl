c0245d0b4cfd04310741515bbae6aad219132f13
supremazx <58238639+supremazx@users.noreply.github.com>
2022-07-20 03:25:21 +0300

    Update 41-redis.sh

--- packages/41-redis.sh
+++ packages/41-redis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LIB_VERSION='6.2.1';
-LIB_URL="https://download.redis.io/releases/redis-${LIB_VERSION}.tar.gz";
+LIB_URL="https://download.redis.io/releases/redis-6.2.1.tar.gz";
 srcDirName=$(downloadFile "$LIB_URL");
 (
   cd "$srcDirName" || exit;
