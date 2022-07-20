1589155351c4706f1fd36bd6cc840070dd9e1a84
supremazx <58238639+supremazx@users.noreply.github.com>
2022-07-20 03:25:40 +0300

    Update 42-php-redis.sh

--- packages/42-php-redis.sh
+++ packages/42-php-redis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LIB_VERSION='5.3.4';
-LIB_URL="https://github.com/phpredis/phpredis/archive/refs/tags/${LIB_VERSION}.tar.gz";
+LIB_URL="https://github.com/phpredis/phpredis/archive/refs/tags/5.3.4.tar.gz";
 LIB_PRIORITY='50';
 LIB_PHP_MODULE_PREFIX_INI='';
 LIB_CONFIGURE_OPTIONS='--prefix=/ --enable-redis-igbinary --enable-redis-msgpack --enable-redis-lz --enable-redis-zstd';
