a8aeea4907daa0a58b25000f1a32a497c1f02c2b
supremazx <58238639+supremazx@users.noreply.github.com>
2022-07-20 03:21:20 +0300

    Update 10-php-msgpack.sh

--- packages/10-php-msgpack.sh
+++ packages/10-php-msgpack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LIB_VERSION='2.1.2';
-LIB_URL="https://github.com/msgpack/msgpack-php/archive/refs/tags/msgpack-${LIB_VERSION}.tar.gz";
+LIB_URL="https://github.com/msgpack/msgpack-php/archive/refs/tags/msgpack-2.1.2.tar.gz";
 LIB_PRIORITY='40';
 LIB_PHP_MODULE_PREFIX_INI='';
 LIB_CONFIGURE_OPTIONS='';
