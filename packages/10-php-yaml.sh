b8226b30e89b1886295b00f2c3037be757a7f87a
supremazx <58238639+supremazx@users.noreply.github.com>
2022-07-20 03:22:33 +0300

    Update 10-php-yaml.sh

--- packages/10-php-yaml.sh
+++ packages/10-php-yaml.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LIB_VERSION='2.0.4';
-LIB_URL="https://github.com/php/pecl-file_formats-yaml/archive/${LIB_VERSION}.tar.gz";
+LIB_URL="https://github.com/php/pecl-file_formats-yaml/archive/2.0.4.tar.gz";
 LIB_PRIORITY='40';
 LIB_PHP_MODULE_PREFIX_INI='';
 LIB_CONFIGURE_OPTIONS='';
