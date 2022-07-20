6cfce251a7b8c777c07d74b810fdae2fcb0deef7
supremazx <58238639+supremazx@users.noreply.github.com>
2022-07-20 03:23:30 +0300

    Update 30-php-phalcon.sh

--- packages/30-php-phalcon.sh
+++ packages/30-php-phalcon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LIB_VERSION='4.1.0';
-LIB_URL="https://github.com/phalcon/cphalcon/archive/v${LIB_VERSION}.tar.gz";
+LIB_URL="https://github.com/phalcon/cphalcon/archive/v4.1.0.tar.gz";
 LIB_PRIORITY='40';
 LIB_PHP_MODULE_PREFIX_INI='';
 LIB_NAME='phalcon';
